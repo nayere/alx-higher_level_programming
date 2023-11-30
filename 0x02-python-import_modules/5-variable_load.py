307d73093676bb23ab85c8426a2358e36432df18
Job  Nayere <37332656+nayere@users.noreply.github.com>
2023-12-01 00:22:27 +0300

    Update 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -2,6 +2,8 @@
 
 if __name__ == "__main__":
     """Print the value of variable a from variable_load_5."""
-    from variable_load_5 import a
-
-    print(a)
+    try:
+        from variable_load_5 import a
+        print(a)
+    except ImportError as e:
+        print(f"Error: {e}. Unable to import variable a from variable_load_5.")
