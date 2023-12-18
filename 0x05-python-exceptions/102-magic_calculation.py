3ec721bb5d10fb107d63420ca7df3fcb266f3a8e
nayere <jobdavy64@gmail.com>
2023-12-18 09:22:39 -0800

    power boost

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(x, y):
+    from magic_calculation_102 import add, sub
 
+    if (x < y):
+        z = add(x, y)
+
+        for w in (range(4, 6)):
+            z = add(z, w)
+        return (z)
+
+    else:
+        return (sub(x, y))
