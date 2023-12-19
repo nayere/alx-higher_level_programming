6ebe11dd896f1ffd44f4f5807947abc13ccab2a3
Job  Nayere <37332656+nayere@users.noreply.github.com>
2023-12-19 23:27:36 +0300

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,13 @@
+#!/usr/bin/python3
+"""This is Square module."""
 
+
+class Square:
+    """This defines a square."""
+
+    def __init__(self, size):
+        """Beginning of Constructor.
+        Args:
+            size: length of the side of the square.
+        """
+        self.__size = size
