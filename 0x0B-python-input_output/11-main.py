15e6bbd0c5e92a7e31da07bc6628b747868f3bb1
nayere <jobdavy64@gmail.com>
2024-01-09 13:16:22 -0800

    Lets Begin

---
+++ 0x0B-python-input_output/11-main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import os
+import sys
+
+Student = __import__('11-student').Student
+read_file = __import__('0-read_file').read_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+path = sys.argv[1]
+
+if os.path.exists(path):
+    os.remove(path)
+
+student_1 = Student("John", "Doe", 23)
+j_student_1 = student_1.to_json()
+print("Initial student:")
+print(student_1)
+print(type(student_1))
+print(type(j_student_1))
+print("{} {} {}".format(student_1.first_name, student_1.last_name, student_1.age))
