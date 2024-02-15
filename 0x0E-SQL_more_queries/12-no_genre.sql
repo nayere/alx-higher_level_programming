f74f725e66faab2b431d19d7692efe0ad2b1a957
Job  Nayere <37332656+nayere@users.noreply.github.com>
2024-02-16 00:50:46 +0300

    Update 12-no_genre.sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,5 @@
+-- this was difficult
+SELECT title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY title, tv_show_genres.genre_id;
