cf7714e315c58590b6303811e9cc81bdc3c8f03c
Job  Nayere <37332656+nayere@users.noreply.github.com>
2024-03-29 00:16:53 +0300

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,18 @@
+#!/usr/bin/python3
+"""
+Takes your GitHub credentials (username and password) and uses the GitHub API
+to display your id
+"""
 
+if __name__ == '__main__':
+    from requests import get
+    from sys import argv
+
+    username = argv[1]
+    password = argv[2]
+
+    URL = "https://api.github.com/nayere"
+    response = get(URL, auth=(nayere, password))
+    json = response.json()
+
+    print(json.get('id'))
