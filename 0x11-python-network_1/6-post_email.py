a41a3cd73043f04ef2c66e344051b6c41cda114f
Job  Nayere <37332656+nayere@users.noreply.github.com>
2024-03-29 00:08:11 +0300

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,16 @@
+#!/usr/bin/python3
+"""
+Sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+By Nayere
+"""
 
+
+if __name__ == '__main__':
+    from sys import argv
+    from requests import post
+
+    url = argv[1]
+    email = argv[2]
+    res = post(url, {'email': email})
+    print(res.text)
