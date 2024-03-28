13f6aad621229e07e1f4266fb4f103b63b19fbf9
Job  Nayere <37332656+nayere@users.noreply.github.com>
2024-03-28 23:47:11 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,6 +2,7 @@
 """Fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
+
 if __name__ == "__main__":
     request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
