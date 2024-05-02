9856487c31a46b6ec44b79a41cb85cfde9756172
Job  Nayere <37332656+nayere@users.noreply.github.com>
2024-05-02 22:02:19 +0300

    Update 100-script.js

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// Select the <header> element using document.querySelector
+// Update the text color of the <header> element to red (#FF0000)
+
+const headerElement = document.querySelector('header');
+
+headerElement.style.color = '#FF0000';
